77a8466f7cc11385bea36570f61a35ac50d911b1
Alan Eicker <Alan.Eicker@allstate.com>
2019-10-03 09:14:12 -0500

    Adds vash template render

--- index.js
+++ index.js
@@ -11,24 +11,27 @@ const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV;
 const md = new MarkdownIt();
 
-// const staticPath = path.join(
-//   __dirname, 
-//   env === 'production' ? '..': '.', 
-//   'public'
-// );
+const staticPath = path.join(
+  __dirname, 
+  env === 'production' ? '..': '.', 
+  'public'
+);
+
+app.set('view engine', 'vash');
+app.set('views', `${__dirname}/views`);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(compression());
 app.use(cors());
-//app.use(express.static(staticPath));
+app.use(express.static(staticPath));
 
 app.get('/', (req, res) => {
   fs.readFile('./markdown.md', 'utf8', (err, data) => {
     if (err) {
       console.log(err);
     }
-    res.send(md.render(data));
+    res.render('index', { markdown: md.render(data) });
   });
 });
 
